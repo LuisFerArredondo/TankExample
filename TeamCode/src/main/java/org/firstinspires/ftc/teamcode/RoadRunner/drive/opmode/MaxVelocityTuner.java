d76d0c220a027d79b0da7119ea01756654e9e33f
LuisFerArredondo <luisfernando.arredondo@gmail.com>
2021-10-07 14:59:55 -0500

    Changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/drive/opmode/MaxVelocityTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/drive/opmode/MaxVelocityTuner.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.RoadRunner.drive.opmode.MecanumDrive;
+package org.firstinspires.ftc.teamcode.RoadRunner.drive.opmode;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/drive/opmode/TurnTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/drive/opmode/TurnTest.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.RoadRunner.drive.opmode.MecanumDrive;
+package org.firstinspires.ftc.teamcode.RoadRunner.drive.opmode;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
